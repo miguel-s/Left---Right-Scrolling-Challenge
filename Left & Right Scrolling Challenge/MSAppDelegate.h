5f93770c941c51e4b3e80ef878f27a02527e4e86
Miguel Serrano <Mimi@Mimi.local>
2014-07-02 22:26:32 +0200

    Initial Commit

---
+++ Left & Right Scrolling Challenge/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  Left & Right Scrolling Challenge
+//
+//  Created by Miguel Serrano on 02/07/14.
+//  Copyright (c) 2014 Miguel Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Left & Right Scrolling Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Left & Right Scrolling Challenge
+//
+//  Created by Miguel Serrano on 02/07/14.
+//  Copyright (c) 2014 Miguel Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
